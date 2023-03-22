08e753bce8e89a32334d58158d6aedccd232df76
Atman Mishra <atmanmishra1122@gmail.com>
2023-03-23 01:04:28 +0530

    modified:   src/pages/ProgressPage/Header.jsx

--- src/pages/ProgressPage/Header.jsx
+++ src/pages/ProgressPage/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <section>
             <div className="py-6 px-6 w-[90%]" style={{ zIndex: 1 }}>
-                <h1 className=" -2xl font-bold lg:text-5xl lg:font-extrabold sm:text-4xl md:text-4xl text-white ">HI, ATMAN MISHRA, HERE IS YOUR COMPLETE PROGRESS REPORT</h1>
+                <h1 className=" -2xl font-bold lg:text-5xl lg:font-extrabold sm:text-4xl md:text-4xl text-white ">HI, ATMAN MISHRA, HERE IS YOUR COMPLETE PROGRESS REPORT and i am a fill</h1>
             </div>
         </section>
     );
